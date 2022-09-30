7205f864717fc9ea9dcb7b452ca2948a68470d61
Kashif Sheikh <kashifshaikh810>
2022-09-30 15:34:25 +0500

    Implement show All Users screen UI with dynamic data --Admin--

--- frontend/src/Screens/Admin/AllUsers/AllUsers.js
+++ frontend/src/Screens/Admin/AllUsers/AllUsers.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {clearErrors, getAdminUsers} from '../../../redux/actions/userAction';
 import AllUsersMarkup from './AllUsersMarkup';
+import {showMessage} from 'react-native-flash-message';
 
 const AllUsers = props => {
-  return <AllUsersMarkup {...props} />;
+  const dispatch = useDispatch();
+  const {loading, users, error} = useSelector(state => state.adminUsers);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    dispatch(getAdminUsers());
+
+    if (error) {
+      showMessage({
+        message: 'Error',
+        description: error,
+        type: 'success',
+      });
+      dispatch(clearErrors());
+    }
+  }, [dispatch, error]);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    dispatch(getAdminUsers());
+    setRefreshing(false);
+  };
+
+  return (
+    <AllUsersMarkup
+      {...props}
+      loading={loading}
+      users={users}
+      refreshing={refreshing}
+      onRefresh={onRefresh}
+    />
+  );
 };
 
 export default AllUsers;
--- frontend/src/Screens/Admin/AllUsers/AllUsersMarkup.js
+++ frontend/src/Screens/Admin/AllUsers/AllUsersMarkup.js
@@ -1,20 +1,109 @@
 import React from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  RefreshControl,
+} from 'react-native';
 import styles from './styles';
 import Header from '../../../components/Layouts/Header/Header';
 import DashboardTopBar from '../../../components/materials/DashboardTopBar/DashboardTopBar';
+import {
+  Table,
+  TableWrapper,
+} from '../../../components/materials/TableComponent/components/table.js';
+import {Cell} from '../../../components/materials/TableComponent/components/cell';
+import {Row} from '../../../components/materials/TableComponent/components/rows';
+import EditIcon from 'react-native-vector-icons/MaterialIcons';
+import DeleteIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const AllUsersMarkup = props => {
+  const headerRow = ['User ID', 'Email', 'Name', 'Role', 'Actions'];
   let param = props?.route?.params?.backRouteName;
 
+  const rows = [];
+
+  props?.users &&
+    props?.users?.forEach((item, i) =>
+      rows.push([item._id, item.email, item.name, item.role, 'button']),
+    );
+
+  const element = (data, index, rowData) => (
+    <View style={styles.elementContainer}>
+      <TouchableOpacity
+        onPress={() => {
+          // props.navigation.navigate('UpdateProduct', {data: rowData[0]});
+        }}>
+        <EditIcon name="edit" size={25} color="#b3b3b3" style={styles.icon} />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      // onPress={() => props.deleteProductOnPressHandler(rowData)}
+      >
+        <DeleteIcon
+          name="delete"
+          size={25}
+          color="#b3b3b3"
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+    </View>
+  );
   return (
     <View style={styles.container}>
       <Header {...props} backRouteName={param ? param : 'Home'} />
 
       <DashboardTopBar {...props} />
 
-      <ScrollView style={styles.scrollView}>
-        <Text>AllUsersMarkup</Text>
+      <ScrollView
+        style={styles.scrollView}
+        refreshControl={
+          <RefreshControl
+            refreshing={props.refreshing}
+            onRefresh={props.onRefresh}
+          />
+        }>
+        <View>
+          <View style={styles.headingContainer}>
+            <Text
+              style={[
+                styles.headingText,
+                {fontFamily: 'AbrilFatface-Regular'},
+              ]}>
+              All Users
+            </Text>
+          </View>
+
+          <View style={styles.table}>
+            <Table borderStyle={styles.tableMain}>
+              <Row
+                data={headerRow}
+                style={styles.head}
+                textStyle={styles.headerText}
+              />
+              {rows.map((rowData, index) => (
+                <TableWrapper key={index} style={styles.row}>
+                  {rowData.map((cellData, cellIndex) => (
+                    <Cell
+                      key={cellIndex}
+                      textStyle={[
+                        styles.text,
+                        cellData === 'user' && {color: 'red'},
+                        cellData === 'admin' && {color: 'green'},
+                      ]}
+                      data={
+                        cellIndex === 4
+                          ? element(cellData, index, rowData)
+                          : cellData
+                      }
+                    />
+                  ))}
+                </TableWrapper>
+              ))}
+            </Table>
+          </View>
+        </View>
       </ScrollView>
     </View>
   );
