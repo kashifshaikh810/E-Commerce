fa130e63f4c596c0e5c699101b4acfebdcaea7d5
Kashif Sheikh <kashifshaikh810>
2022-09-30 19:17:33 +0500

    UpdateUser style minor fix

--- frontend/src/Screens/Admin/UpdateUser/styles.js
+++ frontend/src/Screens/Admin/UpdateUser/styles.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
   picker: tw`border-2 border-gray-400 rounded w-11/12 h-14 self-center mt-5 mb-5 pl-10 pr-5`,
   buttonStyle: tw`w-11/12 h-12 bg-red-400 self-center mb-10 rounded-xl`,
   iconTwo: tw`absolute top-4 left-6 bottom-0 right-0 mt-0.5`,
-  iOSIconTwo: tw`absolute top-24 left-7 bottom-0 right-0 mt-1`,
+  iOSIconTwo: tw`absolute top-24 left-7 bottom-0 right-0 mt-0.5`,
 });
 
 export default styles;
