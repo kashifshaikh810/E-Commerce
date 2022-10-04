0473dd80f76b62690b6e50dd1c6030a752f17c28
Kashif Sheikh <kashifshaikh810>
2022-10-04 19:13:52 +0500

    Implement about us page screen ui

--- frontend/src/Screens/About/About.js
+++ frontend/src/Screens/About/About.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, Image} from 'react-native';
+import styles from './styles';
+import Header from '../../components/Layouts/Header/Header';
+import Footer from '../../components/Layouts/Footer/Footer';
+import Lottie from 'lottie-react-native';
 
-const About = () => {
+const About = props => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={styles.container}>
+      <Header {...props} />
+      <ScrollView>
+        <View style={styles.paddingBottom}>
+          <Text style={styles.aboutUS}>About Us</Text>
+          <Image
+            source={require('../../components/images/ecomTools.jpg')}
+            style={styles.image}
+          />
+          <Text style={styles.text}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Text>
+          <Lottie
+            source={require('../../components/images/109999-animation-for-ecommerce-business-landing-page.json')}
+            autoPlay
+            loop
+            style={styles.lottie}
+          />
+          <Footer {...props} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
--- frontend/src/Screens/About/styles.js
+++ frontend/src/Screens/About/styles.js
@@ -1,5 +1,13 @@
 import {StyleSheet} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: tw`flex-1 bg-white`,
+  paddingBottom: tw`pb-3`,
+  aboutUS: tw`text-4xl text-center pt-4 pb-4`,
+  lottie: tw`h-64 w-full self-center`,
+  text: tw`text-base w-11/12 pt-0 self-center`,
+  image: tw`h-72 w-11/12 self-center mb-5`,
+});
 
 export default styles;
