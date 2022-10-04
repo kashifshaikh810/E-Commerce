c56afb044c8bab5a9d12c30c3a3bb85ea702e7d2
Kashif Sheikh <kashifshaikh810>
2022-10-04 18:23:38 +0500

    Implement contacts us page screen ui

--- frontend/src/Screens/Contact/Contact.js
+++ frontend/src/Screens/Contact/Contact.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
+import styles from './styles';
+import Header from '../../components/Layouts/Header/Header';
+import Footer from '../../components/Layouts/Footer/Footer';
+import Lottie from 'lottie-react-native';
 
-const Contact = () => {
+const Contact = props => {
   return (
-    <View>
-      <Text>Contact</Text>
+    <View style={styles.container}>
+      <Header {...props} />
+      <View style={styles.contactContainer}>
+        <View>
+          <Text
+            style={[
+              styles.textStyle,
+              {fontFamily: 'RubikMarkerHatch-Regular'},
+            ]}>
+            THIS IS ECOMMERCE APP
+          </Text>
+          <Text
+            style={[
+              styles.textStyle,
+              {fontFamily: 'RubikMarkerHatch-Regular'},
+            ]}>
+            FOR ANY QUERY
+          </Text>
+          <Text
+            style={[
+              styles.textStyle,
+              {fontFamily: 'RubikMarkerHatch-Regular'},
+            ]}>
+            CONTACT US : - DEVELOPER01
+          </Text>
+        </View>
+        <Lottie
+          source={require('../../components/images/110986-contact-us.json')}
+          autoPlay
+          loop
+          style={styles.lottie}
+        />
+      </View>
+      <Footer {...props} style={styles.footer} />
     </View>
   );
 };
--- frontend/src/Screens/Contact/styles.js
+++ frontend/src/Screens/Contact/styles.js
@@ -1,5 +1,12 @@
 import {StyleSheet} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: tw`flex-1 bg-white`,
+  contactContainer: tw`flex-1 justify-end items-center pb-10`,
+  textStyle: tw`text-xl text-center pt-2`,
+  lottie: tw`absolute top-0 bottom-32 left-0 right-0`,
+  footer: tw`h-36`,
+});
 
 export default styles;
